863ee19b715042ba77deff21c7f051adc1430103
Lautaro Soffici <lnsoffici@teco.com.ar>
2021-07-06 00:05:19 -0300

    footer changes

--- sections/Footer.jsx
+++ sections/Footer.jsx
@@ -1,15 +1,25 @@
 const Footer = () => {
   return (
     <footer className="flex justify-center font-mono py-5">
-      <a
-        href="https://github.com/lausoffici/portfolio"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <p className="text-sm hover:cursor-[ne-resize] hover-green">
-          Designed by Brittany Chiang and built by me
-        </p>
-      </a>
+      <p className="text-sm flex flex-col sm:flex-row justify-center items-center">
+        <a
+          href="https://brittanychiang.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="mb-2 sm:m-0 hover-green hover:cursor-[ne-resize]"
+        >
+          Designed by Brittany Chiang
+        </a>
+        <div className="hidden sm:block mx-2 cursor-default">|</div>
+        <a
+          href="https://github.com/lausoffici/portfolio"
+          target="_blank"
+          rel="noreferrer"
+          className="hover-green hover:cursor-[ne-resize]"
+        >
+          Built by me
+        </a>
+      </p>
     </footer>
   )
 }
