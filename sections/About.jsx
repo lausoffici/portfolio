0bf58f6ad780041a910f0ede082a60b2e7c44480
Lautaro Soffici <lnsoffici@teco.com.ar>
2021-07-07 23:09:02 -0300

    skills list

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,3 +1,12 @@
+const skills = [
+  'JavaScript (ES6+)',
+  'React',
+  'Node.js',
+  '.NET Core',
+  'SQL',
+  'TypeScript',
+]
+
 const About = () => {
   return (
     <section id="about" className="max-w-[900px]">
@@ -31,6 +40,14 @@ const About = () => {
           ullamcorper, tortor nec venenatis accumsan, purus lacus varius orci,
           ut feugiat tellus eros laoreet orci. Nunc et tempus enim.
         </p>
+        <p>Here are a few technologies I&#39;ve been working with recently:</p>
+        <ul className="skills-list font-mono text-sm mt-6">
+          {skills.map((skillName) => (
+            <li className="mb-2" key={skillName}>
+              {skillName}
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   )
