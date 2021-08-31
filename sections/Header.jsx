83fa517577780186354643b9d84116836ca3a008
Lautaro Soffici <lnsoffici@teco.com.ar>
2021-08-31 01:56:59 -0300

    add header scrolling interaction

--- sections/Header.jsx
+++ sections/Header.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import Button from '../components/Button'
 import Logo from '../public/logo.svg'
+import { useScrollPosition } from '../hooks/useScrollPosition'
 
 const navBarItems = [
   {
@@ -20,29 +22,47 @@ const navBarItems = [
   },
 ]
 
-const Header = () => (
-  <header className="h-[100px] fixed flex justify-between items-center w-full px-[25px] md:px-[50px] font-mono text-sm">
-    <a href="#">
-      <Logo id="logo" width={42} height={42} />
-    </a>
-    <div className="hidden md:flex">
-      <ol className="flex justify-center items-center mr-4">
-        {navBarItems.map((item) => (
-          <li key={item.name} className="mx-1 p-2.5 hover-green">
-            <a href={item.href}>{item.name}</a>
-          </li>
-        ))}
-      </ol>
-      <a href="mailto:lautarosoffici@gmail.com">
-        <Button className="px-4 py-2">Resume</Button>
+const Header = () => {
+  const [sticky, setSticky] = useState(false)
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isShow = currPos.y > prevPos.y
+      if (isShow !== sticky) setSticky(isShow)
+    },
+    [sticky]
+  )
+
+  return (
+    <header
+      className="h-[100px] fixed flex justify-between items-center w-full px-[25px] md:px-[50px] font-mono text-sm bg-[#0a192f]"
+      style={{
+        transform: !sticky ? 'translateY(-100%)' : 'translateY(0)',
+        transition: 'transform 250ms ease-in',
+      }}
+    >
+      <a href="#">
+        <Logo id="logo" width={42} height={42} />
       </a>
-    </div>
-    <div className="flex flex-col md:hidden w-6 h-6">
-      <div className="h-[2px] bg-green mb-1" />
-      <div className="h-[2px] bg-green mb-1" />
-      <div className="h-[2px] bg-green" />
-    </div>
-  </header>
-)
+      <div className="hidden md:flex">
+        <ol className="flex justify-center items-center mr-4">
+          {navBarItems.map((item) => (
+            <li key={item.name} className="mx-1 p-2.5 hover-green">
+              <a href={item.href}>{item.name}</a>
+            </li>
+          ))}
+        </ol>
+        <a href="mailto:lautarosoffici@gmail.com">
+          <Button className="px-4 py-2">Resume</Button>
+        </a>
+      </div>
+      <div className="flex flex-col md:hidden w-6 h-6">
+        <div className="h-[2px] bg-green mb-1" />
+        <div className="h-[2px] bg-green mb-1" />
+        <div className="h-[2px] bg-green" />
+      </div>
+    </header>
+  )
+}
 
 export default Header
